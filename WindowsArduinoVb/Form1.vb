ba5d752d6f1e46d26b396ac1e7b3d98d411a11e1
jeancode <doyer_furia@hotmail.com>
2016-07-29 14:48:03 -0500

    Add files via upload

---
+++ WindowsArduinoVb/Form1.vb
@@ -0,0 +1,53 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        SerialPort1.PortName = "COM9"
+
+        If SerialPort1.IsOpen Then
+
+        Else
+
+            Try
+                SerialPort1.Open()
+            Catch ex As Exception
+
+            End Try
+            Timer1.Start()
+
+            Button1.Enabled = False
+            Button2.Enabled = True
+
+
+        End If
+
+
+
+
+
+    End Sub
+
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+
+        Button1.Enabled = True
+        Button2.Enabled = False
+
+        Timer1.Stop()
+        SerialPort1.Close()
+
+
+
+
+
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+
+        Dim data As String
+        data = SerialPort1.ReadExisting().ToString()
+        RichTextBox1.AppendText(data)
+
+
+    End Sub
+End Class
